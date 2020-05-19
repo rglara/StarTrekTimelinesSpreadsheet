561db619b845464b6f9182ec66d8687270bf3299
Greg Lara <rglara@users.noreply.github.com>
2020-05-18 20:36:43 -0400

    get gallery quick links skeleton in place

--- src/components/factions/FactionDetails.tsx
+++ src/components/factions/FactionDetails.tsx
@@ -19,11 +19,38 @@ export const FactionDetails = () => {
 		);
 	}
 
+	const stringId = (id: number) => `faction${id}`;
+
+	const refs = STTApi.playerData.character.factions.reduce((acc: any, faction) => {
+		acc[stringId(faction.id)] = React.createRef();
+		return acc;
+	}, {} as React.RefObject<HTMLSpanElement>[]);
+
+	const handleClick = (id: number) => refs[stringId(id)].current.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		}
+	);
+
+	const galleryItems: JSX.Element[] = [];
+	const detailItems: JSX.Element[] = [];
+	STTApi.playerData.character.factions.forEach(faction => {
+		galleryItems.push(
+			<div key={faction.id} onClick={() => handleClick(faction.id)}>{faction.name}</div>
+		);
+		detailItems.push(
+			<span ref={refs[stringId(faction.id)]} key={faction.name}>
+				<FactionDisplay faction={faction} />
+			</span>
+		);
+	});
+
 	return (
 		<div className='tab-panel' data-is-scrollable='true'>
-			{STTApi.playerData.character.factions.map(faction => (
-				<FactionDisplay key={faction.name} faction={faction} />
-			))}
+			<div className='faction-gallery'>
+				{galleryItems}
+			</div>
+			{detailItems}
 		</div>
 	);
 }
