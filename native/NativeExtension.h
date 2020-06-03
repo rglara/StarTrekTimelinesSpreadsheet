55e8aae972ace9c34f4fba262ef165c185eb686a
Paul Bilnoski <paul@bilnoski.net>
2020-06-03 15:25:49 -0500

    Remove unneeded native code

--- native/NativeExtension.h
+++
@@ -1,9 +0,0 @@
-#ifndef NATIVE_EXTENSION_H
-#define NATIVE_EXTENSION_H
-
-#include <nan.h>
-
-NAN_METHOD(calculateVoyageRecommendations);
-NAN_METHOD(calculateVoyageCrewRank);
-
-#endif
--- src/api/workertypings.d.ts
+++ src/api/workertypings.d.ts
@@ -1,4 +1,5 @@
-declare module "worker-loader!*" {
-    const content: any;
-    export = content;
-}
+// Already have a WebpackWorker, maybe don't need this one also
+// declare module "worker-loader!*" {
+//     const content: any;
+//     export = content;
+// }
